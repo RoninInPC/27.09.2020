c63e518cfeb83b83432da832ab5371ebc61d9b6c
RoninInPC <62109908+RoninInPC@users.noreply.github.com>
2020-10-14 23:50:00 +0500

    переделка

--- 27.09.2020/main.cpp
+++ 27.09.2020/main.cpp
@@ -22,23 +22,124 @@ std::wstring IntToWStr(int i) {
     std::string str = ss.str();
     return s2ws(str);
 }
-BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
-{
-    MONITORINFOEXA mon;
-    mon.cbSize = sizeof(mon);
-
-    ::GetMonitorInfoA(hMonitor, &mon);
-    int nxRes = GetDeviceCaps(hdcMonitor, HORZRES);
-    int nYRes = GetDeviceCaps(hdcMonitor, VERTRES); 
-    std::wstring Arr = IntToWStr(nxRes) + s2ws("x") + IntToWStr(nYRes)+s2ws("\0");
-    TextOut(hdcMonitor, 0, 0, Arr.c_str(), 8);
+#include <windows.h>
+#include <windowsx.h>
+#include <stdio.h>
+#include <Shellapi.h>
+#include "mmsystem.h"
+#define TIMER_1 12000
+#define SIZE 100
+int size = 0;
+MONITORINFOEX A[SIZE];
+BOOL MonitorEnumProc(HMONITOR monitor, HDC hdc, LPRECT rect, LPARAM data) {
+    MONITORINFOEX info;
+    info.cbSize = sizeof(MONITORINFOEX);
+    GetMonitorInfo(monitor, &info);
+    A[size] = info;
+    size++;
     return TRUE;
 }
-int main()
-{
-    while (1) {
-        HDC common_dc = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
-        ::EnumDisplayMonitors(common_dc, nullptr, MonitorEnumProc, 0);
+RECT glRectField1 = { 0,0,300,300 };
+#define AppWindowName L"Application"
+HINSTANCE hIns;
+HWND    HwndMainWindow;
+ATOM RegisterClassMainWindow();
+HWND CreateMainWindow();
+LRESULT CALLBACK WndProcMain(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
+    MSG msg;
+    hIns = hInstance;
+    HwndMainWindow = CreateMainWindow();
+    SetTimer(HwndMainWindow, TIMER_1, 100, NULL);
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-    return 0;
+
+    return msg.wParam;
+}
+ATOM RegisterClassMainWindow() {
+
+    WNDCLASSEX WndClass;
+    WndClass.cbSize = sizeof(WndClass);
+    WndClass.style = CS_DBLCLKS;
+    WndClass.lpfnWndProc = WndProcMain;
+    WndClass.cbClsExtra = 0;
+    WndClass.cbWndExtra = 0;
+    WndClass.hInstance = hIns;
+    WndClass.hIcon = NULL;
+    WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    WndClass.lpszMenuName = NULL;
+    WndClass.lpszClassName = AppWindowName;
+    WndClass.hIconSm = NULL;
+    return RegisterClassEx(&WndClass);
+}
+HWND CreateMainWindow() {
+
+    HWND hwnd;
+    RegisterClassMainWindow();
+    hwnd = CreateWindowEx(
+        WS_EX_CONTROLPARENT | WS_EX_APPWINDOW,    // расширенный стиль окна
+        AppWindowName,            // имя зарегестрированного класса окна
+        L"27.09.2020.exe",           // заголовок окна
+        WS_OVERLAPPEDWINDOW,      // стиль окна
+        10,                       // X-координата левого верхнего угла
+        10,                       // Y-координата левого верхнего угла
+        300,                      // ширина окна
+        300,                      // высота окна
+        NULL,                     // описатель родительского окна
+       NULL,// описатель главного меню (для главного окна)
+        hIns,
+        NULL);
+
+    if (hwnd == NULL) {
+        MessageBox(NULL, L"Ошибка создания основного окна приложения", L"", MB_OK);
+        return NULL;
+    }
+    ShowWindow(hwnd, SW_SHOWNORMAL);
+    UpdateWindow(hwnd);
+
+    return hwnd;
+}
+LRESULT CALLBACK WndProcMain(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+    HDC	hdc;
+    RECT rect;              // структура задающая прямоугольник
+    PAINTSTRUCT ps;
+    DWORD i;
+    int d;
+    HDC common_dc;
+    switch (iMsg) {
+    case WM_CREATE:
+        
+        return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        hdc = BeginPaint(hwnd, &ps);
+        common_dc = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
+        EnumDisplayMonitors(common_dc, NULL, (MONITORENUMPROC)MonitorEnumProc, NULL);
+        for (int i = 0; i < size; i++) {
+            std::wstring Arr = IntToWStr(A[i].rcMonitor.right) + s2ws("x") + IntToWStr(A[i].rcMonitor.bottom) + s2ws("\0");
+            HFONT hFont = ::CreateFont(50, 0, 0, 0, 0, 0, 0, 0, DEFAULT_CHARSET, 0, 0, 0, 0, L"Arial");
+            SelectObject(hdc, hFont);
+            TextOut(hdc, 50 * i, 0, Arr.c_str(), Arr.size());
+            DeleteObject(hFont);
+        }
+        EndPaint(hwnd, &ps);
+        return 0;
+    case WM_TIMER:
+        size = 0;
+        InvalidateRect(hwnd, &glRectField1, TRUE);
+        return 0;
+    case WM_CLOSE:
+        break;
+    case WM_QUIT:
+        break;
+        return 0;
+    }
+    return DefWindowProc(hwnd, iMsg, wParam, lParam);
 }
